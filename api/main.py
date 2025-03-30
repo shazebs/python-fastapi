72ba85727bc9b7740fbdd3f077763c8fb4aa0649
shazebs <shazebs7@outlook.com>
2025-03-29 23:09:17 -0700

    first commit

---
+++ api/main.py
@@ -0,0 +1,102 @@
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+from ariadne import QueryType, make_executable_schema, graphql_sync
+from ariadne.asgi import GraphQL
+
+
+companies = [
+    {"name": "SpaceX", "ceo": "Elon Musk"},
+    {"name": "Microsoft", "ceo": "Bill Gates"},
+    {"name": "Microsoft", "ceo": "Shazebs"},
+]
+
+# Define GraphQL Schema
+type_defs = """
+    type Company {
+        name: String!
+        ceo: String!
+    }
+
+    type Query {
+        companies(name: String, ceo: String): [Company]!
+    }
+"""
+
+# Define Query Resolver
+query = QueryType()
+
+@query.field("companies")
+def resolve_companies(_, info, name=None, ceo=None):
+    filtered_companies = companies
+    if name:
+        filtered_companies = [company for company in filtered_companies if company["name"].lower() == name.lower()]
+    if ceo:
+        filtered_companies = [company for company in filtered_companies if company["ceo"].lower() == ceo.lower()]
+    return filtered_companies
+
+# Create Executable Schema
+schema = make_executable_schema(type_defs, query)
+
+# FastAPI App
+app = FastAPI()
+
+# Add CORS middleware allowing any origin
+app.add_middleware(
+    CORSMiddleware,
+    # allow_origins=["*"],
+    allow_origins=["http://localhost:5173", "http://sapherons.netlify.app"],
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allow_headers=["*"],
+)
+
+# Add GraphQL Route
+app.add_route("/graphql", GraphQL(schema, debug=True))
+
+class Item(BaseModel):
+    firstName: str = None
+    lastName: str = None
+
+items = []
+
+def helloServer():
+    a = 1
+    b = 2 
+    if (a > b):
+        print(f"{a} is larger than {b}")
+    else:
+        print(f"{a} is smaller than {b}")
+
+
+@app.get("/")
+def root():
+    helloServer()
+    return {"Hello": "World"}
+
+
+@app.get("/items", response_model=list[Item])
+def list_items(limit: int = 10):
+    return items[0:limit]
+
+
+@app.post("/items")
+def create_item(item: Item):
+    items.append(item)
+    return items
+
+
+@app.get("/items/{item_id}", response_model=Item)
+def get_item(item_id: int) -> Item:
+    if item_id < len(items):
+        return items[item_id]
+    else:
+        raise HTTPException(status_code=404, detail="Item not found")
+    
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8000)
+
+# http://127.0.0.1:8000/docs for SwaggerUI 
+# http://127.0.0.1:8000/openapi.json
---
+++ notes.py
@@ -0,0 +1,34 @@
+'''
+Salim-Javed: Writers who wrote screenplays for Hindi Cinema films.
+The duo had superstar appeal and no writer has been able to recreate their level of screenwriter success.
+'''
+
+'''
+"Bait": Creating something others desire, it's information, resources, or social status.
+By offering something that satisfies people's needs or desires, you can manipulate them into coming to you.
+Making your time or resources seem scarce can increase their perceived value, further enticing others to seek you.
+
+Understanding the motivations of others is crucial.
+
+- Establish yourself as an indispensable resource or source of knowledge, skill, or support. This involves identifying what others lack and positioning yourself as the solution.
+- By witholding key information or skills, you ensure that others remain reliant on you.
+
+- Gather intelligence without arousing suspicion. This involves being observant, asking indirect questions, and actively listening to what others say. Be aware of social dynamics, identifying key players, and understanding their motivations.
+- Once you've gathered information, it's crucial to handle it discreetly and avoid revealing your sources. This maintains credibility and prevents others from becoming defensive. 
+
+- The world is in a constant state of change. By isolating yourself, you can become out of touch with those changes, making you vulnerable to those who are more aware.
+
+- Some people are easily offended, while others are more tolerant.
+- Before engaging with someone, it's wise to gather information about their background, reputation, and temperament.
+- Sometimes, people with significant power may appear unassuming. It's crucial to avoid underestimating anyone, as this can lead to disastrous consequences.
+
+- It's crucial to identify the most critical objectives and prioritize your efforts accordingly. Avoid wasting time and energy on trivial matters.
+- Distractions can divert your attention and weaken your focus. It is essential to remain disciplined and avoid being sidetracked.
+
+- A perfect courtier is skilled at subtle flattery, indirect influence, and reading between the lines.
+
+- People are drawn to those who offer them a sense of hope, excitement, or fulfillment. It's about crafting a narrative or image that appeals to people's desires and aspirations.
+- Understand people's deepest desires -- whether it's wealth, power, love, or fame -- is crucial. By offering them the illusion of achieving these desires, you can gain their loyalty and support.
+
+'''
+    
